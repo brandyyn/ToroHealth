efa94498eb1db845e559e966f439b6ec725895b7
danyadev <danyadev@mail.ru>
2025-12-29 20:20:46 +0300

    fix indicating that living entities are hurt when entering another dimension (#5)

--- src/main/java/net/torocraft/torohealthmod/client/event/ToroHealthEventHandler.java
+++ src/main/java/net/torocraft/torohealthmod/client/event/ToroHealthEventHandler.java
@@ -1,10 +1,8 @@
 package net.torocraft.torohealthmod.client.event;
 
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.MathHelper;
-import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.torocraft.torohealthmod.client.particle.DamageParticles;
-import net.torocraft.torohealthmod.mixins.interfaces.EntityLivingBaseExt;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -12,20 +10,8 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class ToroHealthEventHandler {
 
     @SubscribeEvent(priority = EventPriority.LOW)
-    public void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
-        final EntityLivingBase entity = event.entityLiving;
-        if (!entity.worldObj.isRemote) return;
-        final int prevHp = MathHelper.floor_float(((EntityLivingBaseExt) entity).getTorohealth$prevHealth());
-        final int hp = MathHelper.floor_float(entity.getHealth());
-        if (hp > prevHp + 2 && entity.ticksExisted < 5) {
-            // the mob just spawned, and it mistakenly
-            // renders their whole health as healing
-            return;
-        }
-        if (prevHp != hp) {
-            DamageParticles.spawnDamageParticle(entity, prevHp - hp);
-            ((EntityLivingBaseExt) entity).setTorohealth$prevHealth(hp);
-        }
+    public void onLivingHurt(LivingHurtEvent event) {
+        DamageParticles.spawnDamageParticle(event.entityLiving, MathHelper.floor_float(event.ammount));
     }
 
 }
--- src/main/java/net/torocraft/torohealthmod/mixins/interfaces/EntityLivingBaseExt.java
+++
@@ -1,8 +0,0 @@
-package net.torocraft.torohealthmod.mixins.interfaces;
-
-public interface EntityLivingBaseExt {
-
-    int getTorohealth$prevHealth();
-
-    void setTorohealth$prevHealth(int torohealth$prevHealth);
-}
